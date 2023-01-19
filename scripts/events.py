3a5016c74217f5aaa121496019f540c8ddd51f5c
ahmadaminehamdaoui <90623232+ahmadaminehamdaoui@users.noreply.github.com>
2023-01-19 09:02:38 +0100

    Mise à jour de events.py
    Correction de bug.

--- scripts/events.py
+++ scripts/events.py
@@ -21,14 +21,14 @@ def update_event(event):
             # Navigation
             elif event.key == pygame.K_LEFT: cursor.selectedElement.move_cursor(-1) ; cursor.selectedElement.clear_selection()
             elif event.key == pygame.K_RIGHT: cursor.selectedElement.move_cursor(1) ; cursor.selectedElement.clear_selection()
-            elif event.key == pygame.K_UP: cursor.selectedElement.move_line(-1) ; cursor.selectedElement.clear_selection() 
+            elif event.key == pygame.K_UP: cursor.selectedElement.move_line(-1) ; cursor.selectedElement.clear_selection()
             elif event.key == pygame.K_DOWN: cursor.selectedElement.move_line(1) ; cursor.selectedElement.clear_selection()
             # Text input
             elif event.key == pygame.K_TAB:
-                cursor.selectedElement.add_char('TAB')
+                cursor.selectedElement.add_char('TAB', isManual=True)
             else:
                 if event.unicode != '':
-                    cursor.selectedElement.add_char(event.unicode)
+                    cursor.selectedElement.add_char(event.unicode, isManual=True)
 
     # --------------------------- #
     elif event.type == pygame.KEYUP:
