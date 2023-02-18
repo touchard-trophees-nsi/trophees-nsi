7c6bf0d12690c72af0fa45ee430b441d72d5bc42
ahmadaminehamdaoui <90623232+ahmadaminehamdaoui@users.noreply.github.com>
2023-02-18 11:38:25 +0100

    Add files via upload

--- scripts/events.py
+++ scripts/events.py
@@ -7,6 +7,18 @@ def shutdown():
     pygame.quit()
     sys.exit()
 
+def directional_key_update(key, isPressingShift, isPressingCtrl):
+    if key=='LEFT':
+        cursor.selectedElement.move_cursor(-1, shift=isPressingShift, ctrl=isPressingCtrl)
+    elif key=='RIGHT':
+        cursor.selectedElement.move_cursor(1, shift=isPressingShift, ctrl=isPressingCtrl)
+    elif key=='UP':
+        cursor.selectedElement.move_line(-1, shift=isPressingShift)
+    elif key=='DOWN':
+        cursor.selectedElement.move_line(1, shift=isPressingShift)
+    else:
+        print('Invalid direcitonal key')
+
 def update_event(event):
     if event.type == pygame.KEYDOWN:
         # ----- APP SHUTDOWN ----- #
@@ -16,13 +28,20 @@ def update_event(event):
             return 'IS_DEV = not IS_DEV'
         # ----- ENTRY WRITING SYSTEM ----- #
         if cursor.selectedElement!=None and 'Selectable.TextEntry' in cursor.selectedElement.get_type():
-            if event.key == pygame.K_BACKSPACE: cursor.selectedElement.backspace()
+            # Shortcuts
+            isPressingShift = True if event.mod & pygame.KMOD_SHIFT>0 else False
+            isPressingCtrl = True if event.mod & pygame.KMOD_CTRL>0 else False
+            isPressingAlt = True if event.mod & pygame.KMOD_ALT>0 else False
+            if isPressingCtrl and event.key==pygame.K_c: cursor.selectedElement.clipboard_copy()
+            elif isPressingCtrl and event.key==pygame.K_v: cursor.selectedElement.clipboard_paste()
+            # Special Input
+            elif event.key == pygame.K_BACKSPACE: cursor.selectedElement.backspace(ctrl=isPressingCtrl)
             elif event.key == pygame.K_RETURN: cursor.selectedElement.add_return()
             # Navigation
-            elif event.key == pygame.K_LEFT: cursor.selectedElement.move_cursor(-1) ; cursor.selectedElement.clear_selection()
-            elif event.key == pygame.K_RIGHT: cursor.selectedElement.move_cursor(1) ; cursor.selectedElement.clear_selection()
-            elif event.key == pygame.K_UP: cursor.selectedElement.move_line(-1) ; cursor.selectedElement.clear_selection()
-            elif event.key == pygame.K_DOWN: cursor.selectedElement.move_line(1) ; cursor.selectedElement.clear_selection()
+            elif event.key == pygame.K_LEFT: directional_key_update('LEFT', isPressingShift, isPressingCtrl)
+            elif event.key == pygame.K_RIGHT: directional_key_update('RIGHT', isPressingShift, isPressingCtrl)
+            elif event.key == pygame.K_UP: directional_key_update('UP', isPressingShift, isPressingCtrl)
+            elif event.key == pygame.K_DOWN: directional_key_update('DOWN', isPressingShift, isPressingCtrl)
             # Text input
             elif event.key == pygame.K_TAB:
                 cursor.selectedElement.add_char('TAB', isManual=True)
