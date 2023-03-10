709788e0ba12ff72a80ac7c48f038e670420ea21
ahmadaminehamdaoui <90623232+ahmadaminehamdaoui@users.noreply.github.com>
2023-03-10 09:05:51 +0100

    Déplacement du dossier lang
    tropees-nsi/scripts/lang/  ->  trophees-nsi/lang

--- scripts/lang/lang.py
+++
@@ -1,9 +0,0 @@
-lang = 'fr'
-
-with open(lang+'.txt') as file:
-    trads = {'title': (file.readline()).strip(),
-            'subtitle': (file.readline()).strip()}
-
-def getText(txt):
-    return trads[txt] if txt in trads.keys() else None
-
