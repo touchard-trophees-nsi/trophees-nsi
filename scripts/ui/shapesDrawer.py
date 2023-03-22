8208c0fb2184561089ea3cbd326a46989a90c4a2
eti-du <92732299+eti-du@users.noreply.github.com>
2023-03-22 15:39:55 +0100

    Update shapesDrawer.py
    - ajout de nouvelles formes
    - les formes s'alignent maintenant sur la bonne case
    - indication sur l'endroit où se positionnera la forme
    - les formes ne se supperposent plus

--- scripts/ui/shapesDrawer.py
+++ scripts/ui/shapesDrawer.py
@@ -1,28 +1,17 @@
 import copy
 from scripts.math.vector2 import Vector2
 from scripts.ui.shapes import Shape
-defaultShapes = [Shape(Vector2(60,60),Vector2(60,60))]#,Shape(Vector2(120,60),Vector2(60,60))] 
-def updatedDrawer(shapes):
-    if shapes!= [] and shapes[0].pos != Vector2(60,60):
-        shapes.insert(0,Shape(Vector2(60,60),Vector2(60,60),color=(100,110,10)))
 
-def updateDrawer0(shapes):
-    if shapes == []:
-        return list(defaultShapes)
-    else:
-        for i in range(len(defaultShapes)):
-            isinshapes = False
-            for j in range(len(shapes)):
-                print(defaultShapes[i] == shapes[j])
-                if defaultShapes[i] == shapes[j]:
-
-                    isinshapes = True
-            if not isinshapes:
-                shapes.append(defaultShapes[i].copy())
-                
-                print(shapes)
-
-        return shapes
+defaultShapes = [Shape(Vector2(60,60),Vector2(60,60)),
+                Shape(Vector2(60,120),Vector2(60,60),color=(100,110,10), form="square"),
+                Shape(Vector2(120,60),Vector2(60,60),color=(100,110,10),form="triangle",direc=1),
+                Shape(Vector2(180,60),Vector2(60,60),color=(100,110,10),form="triangle",direc=2),
+                Shape(Vector2(120,120),Vector2(60,60),color=(100,110,10),form="triangle",direc=3),
+                Shape(Vector2(180,120),color=(100,110,10),form="triangle"),
+                Shape(Vector2(240,60),form="circle"),
+                Shape(Vector2(300,60),form="circle",direc=1),
+                Shape(Vector2(240,120),form="circle",direc=2),
+                Shape(Vector2(300,120),form="circle",direc=3)]
 
 def updateDrawer(shapes):
     if shapes == []:
@@ -32,3 +21,4 @@ def updateDrawer(shapes):
             if not defaultShapes[i] in shapes:
                 shapes.append(defaultShapes[i].copy())
         return shapes
+    
