93955f94fcc0d87693997eb6f51eaaed5f04adfa
eti-du <92732299+eti-du@users.noreply.github.com>
2023-03-22 16:38:56 +0100

    Update shapes.py
    nettoyage

--- scripts/ui/shapes.py
+++ scripts/ui/shapes.py
@@ -19,49 +19,25 @@ class MoveableElement(Selectable):
         if self.continousClick:
             if self.FirstClickPos == Vector2.ZERO():
                 self.FirstClickPos = Vector2(self.pos.x-cursor.pos.x, self.pos.y-cursor.pos.y)
-            #nshape = self.nearest(shapes,60)
         else:
             self.FirstClickPos = Vector2.ZERO()
         if self.FirstClickPos != Vector2.ZERO():
             self.pos = Vector2(cursor.pos.x+self.FirstClickPos.x, cursor.pos.y+self.FirstClickPos.y)
-    """
-    def onRelease(self):
-        nshape = self.nearest(self.shapes,60)
-        if nshape != None:
-            vectdiff = nshape.pos - self.pos
-            if vectdiff.x < 0 and -min(nshape.size.y,self.size.y)//2 < vectdiff.y < min(nshape.size.y,self.size.y)//2:
-                self.pos = Vector2(nshape.pos.x + self.size.x , nshape.pos.y)
-            elif vectdiff.x > 0 and -min(nshape.size.y,self.size.y)//2 < vectdiff.y < min(nshape.size.y,self.size.y)//2:
-                self.pos = Vector2(nshape.pos.x - self.size.x, nshape.pos.y)
-            elif vectdiff.y < 0:
-                self.pos = Vector2(nshape.pos.x, nshape.pos.y + self.size.y)
-            elif vectdiff.y > 0:
-                self.pos = Vector2(nshape.pos.x, nshape.pos.y - self.size.y)
-        gridspacing = 60 # Valeur actuelle de l'espacement des lignes de la grille
-        self.pos.x -= self.pos.x % gridspacing
-        self.pos.y -= self.pos.y % gridspacing
-    def onRelease(self):
-        gp = 60 # Valeur actuelle de l'espacement des lignes de la grille
-        if True: #[c'est possible de lacher la pièce]:
-            diffx = self.pos.x % gp
-            self.pos.x -= diffx if diffx<gp//2 else diffx-60
-            diffy = self.pos.y % gp
-            self.pos.y -= diffy if diffy<gp//2 else diffy-60
-        else:
-            self.pos = self.lastpos
-        
-        """
         
     def onRelease(self):
-        if self.placeavail(): #[c'est possible de lacher la pièce]:
+        """Lorsque la forme est laché, elle s'aligne avec la grille,
+        si la case sous la pièce n'est vide alors la forme retroune à son emplacement initial"""
+        if self.placeavail():
             self.pos.x,self.pos.y = self.posgrid()
         else:
             self.pos = self.lastpos
+
     def posgrid(self):
+        """revoie les coordonnées de la forme alignée sur la grille"""
         gp = 60 # Valeur actuelle de l'espacement des lignes de la grille
         diffx = self.pos.x % gp
-        posx = self.pos.x - (diffx if diffx<gp//2 else diffx-60)
         diffy = self.pos.y % gp
+        posx = self.pos.x - (diffx if diffx<gp//2 else diffx-60)
         posy = self.pos.y - (diffy if diffy<gp//2 else diffy-60)
         return posx,posy
 
@@ -75,17 +51,8 @@ class MoveableElement(Selectable):
     def onClick(self):
         self.lastpos = self.pos
     
-    """def nearest(self,shapeslist,radius=50):
-        nearestshape,distance = None,radius+1
-        for shape in shapeslist:
-            if shape != self:
-                length = ((shape.pos.x - self.pos.x)**2+(shape.pos.y - self.pos.y)**2)**0.5
-                if length < distance:
-                    nearestshape,distance = shape,length
-        return nearestshape"""
-
 class Shape(MoveableElement):
-    def __init__(self,pos,size=Vector2(60,60),color=(100,0,0),form="square",direc=0) -> None:
+    def __init__(self,pos,size=Vector2(60,60),color=(100,0,0),form="square",direc=0):
         if type(color) == tuple: color = RGB(color)
         super().__init__(pos,size,color)
         self.pos,self.size,self.color = pos,size,color
@@ -94,6 +61,13 @@ class Shape(MoveableElement):
         self.FirstClickPos = Vector2.ZERO()
         self.form = form
         self.direction = direc
+    
+    def __eq__(self,object):
+        if isinstance(object, Shape):
+            return self.pos.x == object.pos.x and self.pos.y == object.pos.y and self.size == object.size
+
+    def copy(self):
+        return Shape(self.pos,self.size,self.color,self.form,self.direction)
 
     def draw(self,screen):
         if self.continousClick and self.placeavail():
@@ -104,35 +78,19 @@ class Shape(MoveableElement):
             pygame.draw.polygon(screen, self.color.value, [(self.pos.x, self.pos.y), (self.pos.x + self.size.x, self.pos.y), (self.pos.x, self.size.y + self.pos.y),(self.pos.x + self.size.x, self.size.y + self.pos.y),(self.pos.x, self.pos.y), (self.pos.x + self.size.x, self.pos.y)][self.direction:self.direction+3], 0)
         elif self.form == "circle":
             pygame.draw.circle(screen, self.color.value, ((self.pos.x+self.size.x,self.pos.x)[self.direction%2], (self.pos.y+self.size.y,self.pos.y)[self.direction//2]), self.size.x, 0, draw_top_left=(True,False,False,False)[self.direction],draw_top_right=(False,True,False,False)[self.direction],draw_bottom_left=(False,False,True,False)[self.direction],draw_bottom_right=(False,False,False,True)[self.direction])
-        
-    def copy(self):
-        return Shape(self.pos,self.size,self.color,self.form,self.direction)
-
-    def __eq__(self,object):
-        if isinstance(object, Shape):
-            return self.pos.x == object.pos.x and self.pos.y == object.pos.y and self.size == object.size
-
 
         """
+    _________triangle_________
+    direc | point 1                         | point 2                        | point 3                        |
         1 | pos.x, pos.y                    | pos.x + size.x, pos.y          | pos.x, pos.y + size.y          |
         2 | pos.x, pos.y                    | pos.x + size.x, pos.y          | pos.x + size.x, pos.y + size.y |
         3 | pos.x, pos.y                    | pos.x + size.x, pos.y + size.y | pos.x, pos.y + size.y          |
         0 | pos.x + size.x, pos.y + size.y  | pos.x + size.x, pos.y          | pos.x, pos.y + size.y          |
 
-        1 | pos.x + size.x, pos.y + size.y |  draw_top_left=True  | draw_top_right=False | draw_bottom_right=False | draw_bottom_left=False |
-        2 | pos.x         , pos.y + size.y |  draw_top_left=False | draw_top_right=True  | draw_bottom_right=False | draw_bottom_left=False |
-        3 | pos.x + size.x, pos.y          |  draw_top_left=False | draw_top_right=False | draw_bottom_right=True  | draw_bottom_left=False |
-        0 | pos.x         , pos.y          |  draw_top_left=False | draw_top_right=False | draw_bottom_right=False | draw_bottom_left=True  |
+    _________cercle_________
+    direc | centre                         |                        partie à dessiner (haut, bas, gauche, droite)                          |
+        1 | pos.x + size.x, pos.y + size.y | draw_top_left=True  | draw_top_right=False | draw_bottom_right=False | draw_bottom_left=False |
+        2 | pos.x         , pos.y + size.y | draw_top_left=False | draw_top_right=True  | draw_bottom_right=False | draw_bottom_left=False |
+        3 | pos.x + size.x, pos.y          | draw_top_left=False | draw_top_right=False | draw_bottom_right=True  | draw_bottom_left=False |
+        0 | pos.x         , pos.y          | draw_top_left=False | draw_top_right=False | draw_bottom_right=False | draw_bottom_left=True  |
         """
-
-"""
-µ to do : 
-- supp double item st incorrc place
-- bin
-µ done:
-- better approximation when release
-- circular shapes
-- when release to an impossible place => return to the initial position
-- shapes drawer adding forms
-- ghost shape
-    """
