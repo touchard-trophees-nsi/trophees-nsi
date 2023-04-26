63a4d45093eb0bf1f7f67c6b95629e27a215c9b2
froskie-hmd <90623232+froskie-hmd@users.noreply.github.com>
2023-04-26 21:01:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 # TO-DO
 
-# SHAPES COLLISION
-
 # LATER: add suport for syntax highlighting when horizontally scrolling (strings & comments)
 # LATER: selection highlight can't be drawn further than the panel boundaries
 # LATER: add numbers highlight, string highlight should be ended with couples ('' or "" and not '" or "')
-
-# DONE : panel order management depending on last clicked panel
-# DONE : add maintained key press support for all keys, including BACKSPACE and RETURN
-# DONE : Ctrl-x should copy (and not juste delete)
-# DONE : add Ctrl-A
-# DONE : selection shouldn't be removed when clicking with the cursor further than the line span
-# DONE : maintained arrow press for faster navigation
-# DONE : huge optimization for ColoredLabel class
-# DONE : optimizing huge amounts of lines
-# DONE : cursor can't be drawn further than the panel boundaries
 
 # MODULES
 import pygame, sys
@@ -22,13 +10,18 @@ from scripts.cursor import cursor
 from scripts.events import update_event, keys, key_update
 from scripts.math.vector2 import Vector2, vectorize
 from scripts.math.camera import camera
-from scripts.ui.panel import Panel, TextPanel, TopNavPanel, update_panel_buttons
-from scripts.ui.label import Label
+from scripts.ui.panel import TextPanel, TopNavPanel, update_panel_buttons
 from scripts.ui.grid import grid
-from scripts.components.shapesDrawer import updateDrawer
+from scripts.components.shapes import update_shape_dragging
 from scripts.components.PCB import PCB
-from scripts.dev import dev_update_and_draw, dev_update
-from scripts.version import PYGAME_VERSION
+from scripts.components.CPU import CPU
+from scripts.components.microcontroller import Microcontroller
+from scripts.components.videochip import VideoChip
+from scripts.components.screen import Screen
+from scripts.dev import dev_update_and_draw, dev_update, dprint
+from scripts.parser.pseudocode import get_game_state, get_code, update_code
+from scripts.parser.user_print import user_print,dynamic_labels
+from scripts.parser.exception_handler import handle_exception
 
 IS_DEV = False
 MAX_FPS = 60
@@ -37,16 +30,16 @@ MAX_FPS = 60
 pygame.init()
 pygame.display.init()
 pygame.mixer.init()
-pygame.display.set_caption('game')
+pygame.display.set_caption('Micro Gadgets')
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 #pygame.mouse.set_visible(0)
 
 # variables
 panels = [TextPanel(Vector2(0,0), Vector2(500,500)), TopNavPanel(Vector2(camera.w_2-80, 0), Vector2(160, 40))]
-fpsLabel = Label(Vector2(0,-4), color=(0,255,0))
-shapes = [PCB(Vector2(0,0))]
+shapes = [Screen(Vector2(800,900)),VideoChip(Vector2(900,100)),Microcontroller(Vector2(700,700)),CPU(Vector2(600,600)), PCB(Vector2(0,0))]
 lastPressed = None
+oldGameState = False
 
 # MAIN LOGIC
 while True:
@@ -58,6 +51,20 @@ while True:
 
     # ----- instance updates ----- #
     cursor.update()
+
+    try:
+        if get_game_state() and oldGameState==False: # >>update loop
+            # >>start execution
+            user_print('Successfully executed code!')
+            exec(get_code()[0])
+        elif get_game_state() and oldGameState==True:                                         
+            exec(get_code()[1])
+        elif not get_game_state() and oldGameState==True:
+            user_print('Terminated code!')
+            exec(get_code()[2])
+    except Exception as e:
+        user_print(handle_exception(e))
+    oldGameState = get_game_state()
 
     # ------- event handler ------- #
     for event in pygame.event.get():
@@ -102,6 +109,7 @@ while True:
     # ----- shape updates ----- #
     for shape in shapes:
         shape.update(shapes)
+    update_shape_dragging(shapes)
     #updateDrawer(shapes)
 
     # ----- panel updates ----- #
@@ -117,6 +125,9 @@ while True:
         shape.draw(screen)
     for panel in panels:
         panel.draw(screen)
+    for label in dynamic_labels:
+        label.draw(screen, label.text)
+        if label.color == (0,0,0): dynamic_labels.remove(label)
 
     if IS_DEV==True:
         dev_update_and_draw(screen, currentFps, MAX_FPS, lastPressed)
