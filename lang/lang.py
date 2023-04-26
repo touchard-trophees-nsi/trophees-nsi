604c17cc08f93df86aa9e3b4302fdfad5bb41053
axelgallard <90622695+axelgallard@users.noreply.github.com>
2023-04-26 13:06:23 +0200

    Update lang.py

--- lang/lang.py
+++ lang/lang.py
@@ -1,9 +1,13 @@
-lang = 'fr'
+import yaml
+
+lang='fr'
+
+def getText(key,lang):
+    with open("lang/trads/"+lang+".yaml", 'r',encoding="utf8") as read_file:      
+        data = yaml.safe_load(read_file)
+    if key in data:
+        return data[key]
+    else:
+        return ""
+
 isKeyboardAzerty = False
-'''
-with open(lang+'.txt') as file:
-    trads = {'title': (file.readline()).strip(),
-            'subtitle': (file.readline()).strip()}
-'''
-def getText(txt):
-    return trads[txt] if txt in trads.keys() else None
