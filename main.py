797c81fdfc6cab648c8f51c093ef9f110841984d
froskie-hmd <90623232+froskie-hmd@users.noreply.github.com>
2023-04-28 20:38:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,11 @@ from scripts.cursor import cursor
 from scripts.events import update_event, keys, key_update
 from scripts.math.vector2 import Vector2, vectorize
 from scripts.math.camera import camera
-from scripts.ui.panel import TextPanel, TopNavPanel, update_panel_buttons
+from scripts.ui.panel import AddComponentPanel, TextPanel, TopNavPanel, update_panel_buttons
 from scripts.ui.grid import grid
 from scripts.components.shapes import update_shape_dragging
-from scripts.components.PCB import PCB
-from scripts.components.CPU import CPU
-from scripts.components.microcontroller import Microcontroller
-from scripts.components.videochip import VideoChip
-from scripts.components.screen import Screen
 from scripts.dev import dev_update_and_draw, dev_update, dprint
-from scripts.parser.pseudocode import get_game_state, get_code, update_code
+from scripts.parser.pseudocode import get_game_state, get_code, set_game_state, stop_game
 from scripts.parser.user_print import user_print,dynamic_labels
 from scripts.parser.exception_handler import handle_exception
 
@@ -31,20 +26,34 @@ pygame.init()
 pygame.display.init()
 pygame.mixer.init()
 pygame.display.set_caption('Micro Gadgets')
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+_SCREEN = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 #pygame.mouse.set_visible(0)
 
-# variables
-panels = [TextPanel(Vector2(0,0), Vector2(500,500)), TopNavPanel(Vector2(camera.w_2-80, 0), Vector2(160, 40))]
-shapes = [Screen(Vector2(800,900)),VideoChip(Vector2(900,100)),Microcontroller(Vector2(700,700)),CPU(Vector2(600,600)), PCB(Vector2(0,0))]
+# variables & functions
+panels = [AddComponentPanel(Vector2(120,120), Vector2(280,240)), TopNavPanel(Vector2(camera.w_2-80, 0), Vector2(160, 40))]
+shapes = []
 lastPressed = None
 oldGameState = False
+oldTimer = 0
+
+def get_component_from_ID(ID):
+    global shapes
+    for shape in shapes:
+        if shape.id == ID:
+            if 'Screen' in shape.get_type():
+                for shape_ in shapes:
+                    if 'VideoChip' in shape_.get_type() and shape_.id == shape.get_videoChipID():
+                        break
+                else:
+                    user_print('ID de la puce vidéo de {} inexistante'.format(shape.id))
+                    set_game_state(False)
+            return shape
 
 # MAIN LOGIC
 while True:
     # --- frame init --- #
-    screen.fill((0,0,0))
+    _SCREEN.fill((0,0,0))
     cursor.pos = vectorize(pygame.mouse.get_pos())
     currentFps = round(clock.get_fps(),1)
     dev_update(currentFps, MAX_FPS)
@@ -55,16 +64,18 @@ while True:
     try:
         if get_game_state() and oldGameState==False: # >>update loop
             # >>start execution
-            user_print('Successfully executed code!')
             exec(get_code()[0])
+            oldTimer = 0
         elif get_game_state() and oldGameState==True:                                         
             exec(get_code()[1])
-        elif not get_game_state() and oldGameState==True:
-            user_print('Terminated code!')
+        elif not get_game_state() and oldTimer<2:
             exec(get_code()[2])
     except Exception as e:
         user_print(handle_exception(e))
+        stop_game()
     oldGameState = get_game_state()
+    if oldGameState == False and oldTimer<2:
+        oldTimer += 1
 
     # ------- event handler ------- #
     for event in pygame.event.get():
@@ -75,26 +86,25 @@ while True:
         cursor.isClicking -= 1 if cursor.isClicking > 0 else 0
         cursor.isReleasing -= 1 if cursor.isReleasing > 0 else 0
         if event.type == pygame.MOUSEBUTTONUP:
-            cursor.isReleasing = 2
+            cursor.isReleasing = 1
             cursor.set_eventType(0)
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            cursor.isClicking = 2
+            cursor.isClicking = 1
             cursor.set_eventType(event.button)
 
             # SCROLLING
             if event.button==4: # scroll up
-                if 'Selectable.TextEntry' in cursor.selectedElement.get_type() and cursor.selectedElement.displayedLines[0]>0:
+                if cursor.selectedElement != None and 'Selectable.TextEntry' in cursor.selectedElement.get_type() and cursor.selectedElement.displayedLines[0]>0:
                     cursor.selectedElement.displayedLines[0]-=1
                     cursor.selectedElement.displayedLines[1]-=1
                     cursor.selectedElement.update_labels()
             elif event.button==5: # scroll down
-                if 'Selectable.TextEntry' in cursor.selectedElement.get_type() and cursor.selectedElement.displayedLines[0]<len(cursor.selectedElement.content)-1:
+                if cursor.selectedElement != None and 'Selectable.TextEntry' in cursor.selectedElement.get_type() and cursor.selectedElement.displayedLines[0]<len(cursor.selectedElement.content)-1:
                     cursor.selectedElement.displayedLines[0]+=1
                     cursor.selectedElement.displayedLines[1]+=1
                     cursor.selectedElement.update_labels()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_F3:
-                print(IS_DEV, IS_DEV==False, IS_DEV==True)
                 IS_DEV = not IS_DEV #False if IS_DEV==False else True
             lastPressed = event.unicode
         elif event.type == pygame.KEYUP:
@@ -109,7 +119,7 @@ while True:
     # ----- shape updates ----- #
     for shape in shapes:
         shape.update(shapes)
-    update_shape_dragging(shapes)
+    update_shape_dragging(shapes, panels)
     #updateDrawer(shapes)
 
     # ----- panel updates ----- #
@@ -122,15 +132,16 @@ while True:
     grid(181,(80,80,80))
 
     for shape in shapes:
-        shape.draw(screen)
+        if (shape.get_showWhenRunning() and get_game_state()) or not get_game_state():
+            shape.draw(_SCREEN)
     for panel in panels:
-        panel.draw(screen)
+        panel.draw(_SCREEN)
     for label in dynamic_labels:
-        label.draw(screen, label.text)
+        label.draw(_SCREEN, label.text)
         if label.color == (0,0,0): dynamic_labels.remove(label)
 
     if IS_DEV==True:
-        dev_update_and_draw(screen, currentFps, MAX_FPS, lastPressed)
+        dev_update_and_draw(_SCREEN, currentFps, MAX_FPS, lastPressed)
 
     # --- screen refreshing ---#
     pygame.display.update()
